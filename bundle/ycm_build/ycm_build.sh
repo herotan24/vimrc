e67fea222f3766c47d28c60673d06e07a9e0fe9b
herotan24 <yuhua_tan24@sina.com>
2019-12-26 01:05:50 +0800

    don't use python2

--- bundle/ycm_build/ycm_build.sh
+++ bundle/ycm_build/ycm_build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=/tmp/llvm . ../YouCompleteMe/third_party/ycmd/cpp
+cmake -G "Unix Makefiles" \
+      -DCMAKE_CXX_COMPILER=/usr/bin/gcc \
+      -DPATH_TO_LLVM_ROOT=/usr/lib/llvm-3.9/ \
+      -DPYTHON_INCLUDE_DIR=/usr/include/python3.6 \
+      -DPYTHON_LIBRARY=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu/libpython3.6.so \
+      -DUSE_PYTHON2=OFF \
+      . \
+      ../YouCompleteMe/third_party/ycmd/cpp
+
 cmake --build . --target ycm_core --config Release
