73bb9d260f142f266b1463c88145b08bf4cca841
herotan24 <yuhua_tan24@sina.com>
2022-01-08 01:35:16 +0800

    script bugfix

--- vim/build/ubuntu/2nd_build_vim.sh
+++ vim/build/ubuntu/2nd_build_vim.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 ./configure --with-features=huge \
-            --enable-pythoninterp \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=/usr/lib/python3.8/config-3.8-x86_64-linux-gnu \
             --enable-gui=auto \
             --enable-cscope \
             --enable-multibyte \
-            --prefix=/usr \
+            --prefix=/usr/local \
 
 make VIMRUNTIMEDIR=/usr/share/vim/vim80
 make install
