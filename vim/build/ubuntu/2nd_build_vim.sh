040b5f646f2dfb1f912765d51c78f493e744c5ab
herotan24 <yuhua_tan24@sina.com>
2023-05-25 00:33:49 +0800

    update vim

--- vim/build/ubuntu/2nd_build_vim.sh
+++ vim/build/ubuntu/2nd_build_vim.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+export LDFLAGS="-rdynamic"
+
 export INSTALL_DIR=/usr/local
 
 ./configure --with-features=huge \
-            --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            --enable-pythoninterp=no \
+            --with-python-config-dir=/usr/lib64/python2.7/config/ \
             --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.8/config-3.8-x86_64-linux-gnu \
+            --with-python3-config-dir=/usr/local/lib/python3.11/config-3.11-x86_64-linux-gnu \
             --enable-gui=auto \
             --enable-cscope \
             --enable-multibyte \
             --prefix=${INSTALL_DIR} \
 
-make VIMRUNTIMEDIR=${INSTALL_DIR}/share/vim/vim90
+make VIMRUNTIMEDIR=${INSTALL_DIR}/share/vim/vim90 -j
 make install
 
 ln -s /usr/local/bin/vim /usr/local/bin/vi
