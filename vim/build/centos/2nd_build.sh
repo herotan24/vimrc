357d4391a097904edfb614847773ac1b70940141
herotan24 <yuhua_tan24@sina.com>
2024-03-29 15:57:09 +0800

    for centos

--- vim/build/centos/2nd_build.sh
+++ vim/build/centos/2nd_build.sh
@@ -4,17 +4,18 @@ export LDFLAGS="-rdynamic"
 
 export INSTALL_DIR=/usr/local
 
+make distclean
 ./configure --with-features=huge \
-            --enable-pythoninterp=no \
-            --with-python-config-dir=/usr/lib64/python2.7/config/ \
             --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/local/lib/python3.11/config-3.11-x86_64-linux-gnu \
+            --with-python3-config-dir=$(python3-config --configdir) \
             --enable-gui=auto \
             --enable-cscope \
             --enable-multibyte \
-            --prefix=${INSTALL_DIR} \
+            --prefix=${INSTALL_DIR}
 
-make distclean
+#--enable-pythoninterp=yes \
+#--with-python-config-dir=/usr/lib64/python2.7/config/ \
+
 make VIMRUNTIMEDIR=${INSTALL_DIR}/share/vim/vim90 -j2
 make install
 
