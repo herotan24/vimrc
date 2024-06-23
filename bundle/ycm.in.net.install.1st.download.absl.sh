3fb9f64b58215ced459fe664fb6b9726ab6b60ea
herotan24 <yuhua_tan24@sina.com>
2024-06-24 03:02:48 +0800

    dont track absl

--- bundle/ycm.in.net.install.1st.download.absl.sh
+++ bundle/ycm.in.net.install.1st.download.absl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-git clone https://github.com/abseil/abseil-cpp.git YouCompleteMe/third_party/ycmd/cpp/absl/
-cd YouCompleteMe/third_party/ycmd/cpp/absl/
+git clone https://github.com/abseil/abseil-cpp.git absl
+cd absl
 git checkout 3b4a16abad2c2ddc494371cc39a2946e36d35d11
 cd -
