d6105aa48bca5c3ee68b02bf3474fb497b3ff59a
herotan24 <yuhua_tan24@sina.com>
2024-06-24 01:38:22 +0800

    添加内网编译 yum 脚本

---
+++ bundle/ycm.in.net.install.2nd.use.local.absl.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# 定义要修改的文件
+FILE="YouCompleteMe/third_party/ycmd/cpp/CMakeLists.txt"
+
+# 检查文件是否存在
+if [ ! -f "$FILE" ]; then
+    echo "Error: 文件 ${FILE} 不存在."
+    exit 1
+fi
+
+# 替换掉下载 ABSEIL 的操作
+# 使用awk处理多行模式匹配和替换，并检查是否有匹配
+found=0
+awk '
+BEGIN { skip = 0 }
+/^if \( USE_SYSTEM_ABSEIL \)/ { found=1; print "#"$0; skip = 1; next }
+/^endif()/ {
+    if (skip) {
+        print "#"$0;
+        print "add_subdirectory(absl)";
+        skip = 0;
+        next;
+    }
+}
+{ if (skip) print "#"$0; else print $0 }
+END { if (!found) print "NO_MATCH" }
+' $FILE > tmpfile
+
+# 检查是否有匹配的内容
+if grep -q "NO_MATCH" tmpfile; then
+    echo "Error: 未找到匹配的内容。"
+    rm tmpfile
+    exit 1
+else
+    mv tmpfile $FILE
+    echo "操作完成。"
+fi
+
---
+++ bundle/ycm.in.net.install.3rd.download.clang.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+echo "如果命令行下载不成功，用浏览器下载后，放置到指定目录"
+
+wget https://github.com/ycm-core/llvm/releases/download/14.0.0/libclang-14.0.0-x86_64-unknown-linux-gnu.tar.bz2 ./YouCompleteMe/third_party/ycmd/clang_archives/
