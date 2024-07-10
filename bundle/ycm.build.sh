774f6fe8dfc9844b6777bf197fe04aa0a4c485ee
herotan24 <yuhua_tan24@sina.com>
2024-07-10 16:44:13 +0800

    解决 mac 编译失败问题

--- bundle/ycm.build.sh
+++ bundle/ycm.build.sh
@@ -2,6 +2,12 @@
 
 cd ./YouCompleteMe
 
+# use pyenv to install pythong devel
+# pyenv install 3.8.19
+# 然后在需要编译的路径配置 python
+# pyenv local 3.8.19
+# 编译 vim 的时候同样需要
+
 while true
 do
     #./install.py --clangd-completer --go-completer --java-completer --verbose
--- vim/build/mac/build_vim.sh
+++ vim/build/mac/build_vim.sh
@@ -8,13 +8,21 @@ export INSTALL_DIR=/usr/local
             --enable-cscope \
             --enable-multibyte \
             --prefix=${INSTALL_DIR} \
-            --with-python3-config-dir=/Users/tanyuhua/.pyenv/versions/3.9.6/lib/python3.9/config-3.9-darwin/
+            --with-python3-config-dir=/Users/tanyuhua/.pyenv/versions/3.8.19/lib/python3.8/config-3.8-darwin
+
+            #--with-python3-config-dir=/opt/homebrew/opt/python@3.12/Frameworks/Python.framework/Versions/3.12/lib/python3.12/config-3.12-darwin
 
 # use pyenv to install pythong devel
-# pyenv install 3.9.6
+# pyenv install 3.8.19
+# 然后在需要编译的路径配置 python
+# pyenv local 3.8.19
+# 编译 Ycm 的时候同样需要
 
-make VIMRUNTIMEDIR=${INSTALL_DIR}/share/vim/vim90
-make install
+make VIMRUNTIMEDIR=${INSTALL_DIR}/share/vim/vim90 -j
+make -j install
 
-rm -rf /usr/local/bin/vi
-ln -s /usr/local/bin/vim /usr/local/bin/vi
+# 如果出现 YouCompleteMe unavailable: dlopen(/Users/tanyuhua/.pyenv/versions/3.8.19/lib/python3.8/lib-dynload/_socket.cpython-38-darwin.so, 0x0002): symbol not found in flat namespace '_PyByteArray_Type'
+# 尝试重拷贝 /usr/local/bin/vim (make install 中有 strips 操作)
+cp -rf ./src/vim /usr/local/bin/vim
+
+ln -sf /usr/local/bin/vim /usr/local/bin/vi
