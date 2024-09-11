994a70b89616b355a530eedc3ed3d4a1eac198f3
herotan24 <yuhua_tan24@sina.com>
2024-09-11 14:25:33 +0800

    添加 ycm conf 样例

---
+++ scripts/YouCompleteMe/python.ycm_extra_conf.py
@@ -0,0 +1,7 @@
+def Settings( **kwargs ):
+    return {
+            'interpreter_path': '/opt/homebrew//Cellar/python@3.8/3.8.19/libexec/bin/python3',
+            'sys_path': [
+                '/opt/homebrew/lib/python3.8/site-packages/puremvc',
+                ]
+            }
